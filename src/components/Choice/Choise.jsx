25a60b438b55e1c6f857209b69c33f3aaba3db9b
BenjaminLG <bleguellec.dev@gmail.com>
2024-05-22 22:16:46 +0200

    :hammer: adding input ratio personnage

---
+++ src/components/Choice/Choise.jsx
@@ -0,0 +1,18 @@
+
+import './Choise'
+
+function Choise() {
+  
+
+  return (
+    <>
+      <section className=''>
+        <input type="radio"  name="Choix_du_personnage" value="Personnage"/>
+        <label htmlFor="html">Personnage</label>
+        
+      </section>
+    </>
+  )
+}
+
+export default Choise
