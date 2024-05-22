90696bdd83e0385f7a3726732702e47b64fa0aa4
BenjaminLG <bleguellec.dev@gmail.com>
2024-05-22 22:23:11 +0200

    :hammer: adding choise input and label

--- src/components/Choice/Choise.jsx
+++ src/components/Choice/Choise.jsx
@@ -6,10 +6,13 @@ function Choise() {
 
   return (
     <>
-      <section className=''>
-        <input type="radio"  name="Choix_du_personnage" value="Personnage"/>
-        <label htmlFor="html">Personnage</label>
-        
+      <section className='choise'>
+        <input className='choise__input' type="radio"  name="Choix_du_personnage" value="Personnage"/>
+        <label className='choise__label' htmlFor="personnage">Personnage</label>
+        <input className='choise__input' type="radio"  name="Choix_action" value="Action"/>
+        <label className='choise__label' htmlFor="action">Action</label>
+        <input className='choise__input' type="radio"  name="Choix_du_lieu" value="Lieu"/>
+        <label className='choise__label' htmlFor="lieu">Lieu</label>
       </section>
     </>
   )
