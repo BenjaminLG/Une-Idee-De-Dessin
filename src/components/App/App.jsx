4402402ba84caceefa59de666a01064282630002
BenjaminLG <bleguellec.dev@gmail.com>
2024-05-22 22:17:38 +0200

    Choise integration in APP

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from '../Header/Header'
 import Instruction from '../Instruction/Instruction'
+import Choise from '../Choice/Choise'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Instruction />
+      <Choise />
     </>
   )
 }
