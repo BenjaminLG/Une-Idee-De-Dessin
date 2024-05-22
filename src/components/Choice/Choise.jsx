21c6e14d43534adc02f27248a8900d8fb813c14b
BenjaminLG <bleguellec.dev@gmail.com>
2024-05-22 22:36:09 +0200

    :hammer: adding button for choise validation

--- src/components/Choice/Choise.jsx
+++ src/components/Choice/Choise.jsx
@@ -7,12 +7,15 @@ function Choise() {
   return (
     <>
       <section className='choise'>
-        <input className='choise__input' type="radio"  name="Choix_du_personnage" value="Personnage"/>
-        <label className='choise__label' htmlFor="personnage">Personnage</label>
-        <input className='choise__input' type="radio"  name="Choix_action" value="Action"/>
-        <label className='choise__label' htmlFor="action">Action</label>
-        <input className='choise__input' type="radio"  name="Choix_du_lieu" value="Lieu"/>
-        <label className='choise__label' htmlFor="lieu">Lieu</label>
+        <article>
+          <input className='choise__input' type="radio"  name="Choix_du_personnage" value="Personnage"/>
+          <label className='choise__label' htmlFor="personnage">Personnage</label>
+          <input className='choise__input' type="radio"  name="Choix_action" value="Action"/>
+          <label className='choise__label' htmlFor="action">Action</label>
+          <input className='choise__input' type="radio"  name="Choix_du_lieu" value="Lieu"/>
+          <label className='choise__label' htmlFor="lieu">Lieu</label>
+        </article>
+        <button>Générer</button>
       </section>
     </>
   )
